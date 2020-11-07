fceda2fa4f4c13fa99fe827c504796e5175549cc
Tom Bell <tomb@tomb.io>
2020-11-07 22:18:36 +0000

    Add internal crypto package

---
+++ internal/crypto/crypto.go
@@ -0,0 +1,22 @@
+package crypto
+
+import (
+	"fmt"
+
+	"github.com/andreburgaud/crypt2go/ecb"
+	"golang.org/x/crypto/blowfish"
+)
+
+// Decrypt decrypts the given cipher text using the given key.
+func Decrypt(ct, key []byte) ([]byte, error) {
+	block, err := blowfish.NewCipher(key)
+	if err != nil {
+		return nil, fmt.Errorf("new blowfish cipher failed: %w", err)
+	}
+
+	mode := ecb.NewECBDecrypter(block)
+	pt := make([]byte, len(ct))
+	mode.CryptBlocks(pt, ct)
+
+	return pt, nil
+}
